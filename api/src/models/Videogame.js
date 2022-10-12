5e900a561fcedd327b31777d3ba4d8db733ff117
Miguel Ángel Beltrán Beltrán <miguelio@Mac-mini-de-Miguel-2.local>
2022-10-11 22:28:33 -0500

    moduls

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -2,11 +2,48 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+	// defino el modelo
+	sequelize.define('videogame', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			allowNull: false,
+			primaryKey: true,
+		},
+
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+
+		released: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+
+		rating: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+
+		platforms: {
+			type: DataTypes.ARRAY(DataTypes.TEXT),
+			allowNull: false,
+		},
+
+		background_image: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+
+		createdInDb: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+		},
+	});
 };
