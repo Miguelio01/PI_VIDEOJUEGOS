9d33a64f4b54c2e6b4a42c7e4a31986de7f8e128
Miguel A.Beltran <61295071+Miguelio01@users.noreply.github.com>
2022-10-13 10:29:30 -0500

    json

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,40 @@
 const { Router } = require('express');
+require('dotenv').config();
+const { API_KEY } = process.env;
+const axios = require('axios');
+const { Videogame, Genre } = require('../db.js');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
 
+const getApiInfo = async () => {
+	let videoArr = [];
+	let urlApi = `https://api.rawg.io/api/games?key=${API_KEY}`;
+
+	try {
+		for (let i = 0; i < 5; i++) {
+			const urlData = await axios.get(urlApi);
+			urlData.data.results.map((e) => {
+				videoArr.push({
+					id: e.id,
+					name: e.name,
+					image: e.background_image,
+					released: e.released,
+					rating: e.rating,
+					platforms: e.platforms.map((e) => e.platform.name),
+					genres: e.genres.map((e) => e.name),
+				});
+			});
+			urlApi = urlData.data.next;
+		}
+		return videoArr;
+	} catch (error) {
+		console.log(error);
+	}
+};
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
 
 module.exports = router;
