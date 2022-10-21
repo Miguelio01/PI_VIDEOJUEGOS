fd1ede16a8c8be6c539e217090c8ce0095b859a8
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-10-20 19:47:48 -0500

    Cards

--- client/src/components/card/Car.jsx
+++ client/src/components/card/Car.jsx
@@ -6,7 +6,7 @@ export default function Car({ id, background_image, name, genres }) {
 	return (
 		<div className={s.card}>
 			<div className={s.img}>
-				<img src={background_image} alt='' />
+				<img src={background_image} alt='' height='100px' />
 			</div>
 			<div className={s.info}>
 				<h3>{name}</h3>
--- client/src/components/home/HomePage.jsx
+++ client/src/components/home/HomePage.jsx
@@ -14,18 +14,17 @@ import { Link } from 'react-router-dom';
 import Car from '../card/Car';
 import Paginado from '../Paginado/Paginado';
 
-
 export default function HomePage() {
 	const dispatch = useDispatch();
 	const allVideogames = useSelector((state) => state.videogames);
 	const allGenres = useSelector((state) => state.genres);
 	const [currentPage, setCurrentPage] = useState(1);
-	const [orden,setOrden] = useState('')
+	const [orden, setOrden] = useState('');
 	const [gamesPerPage] = useState(15);
 	const indexOfLastGame = currentPage * gamesPerPage;
 	const indexOfFirstGame = indexOfLastGame - gamesPerPage;
 	const currentGames = allVideogames.slice(indexOfFirstGame, indexOfLastGame);
-	
+
 	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
 	useEffect(() => {
@@ -35,10 +34,50 @@ export default function HomePage() {
 
 	function handleClick(e) {
 		e.preventDefault();
-		dispatch(getVideogames();)
+		dispatch(getVideogames());
 	}
 
-	
+	function handleFilterGenre(e) {
+		dispatch(filterByGenre(e.target.value));
+		setCurrentPage(1);
+	}
 
-	return <div className={s.backg}></div>;
+	function handleFilterCreated(e) {
+		dispatch(filterByCreated(e.target.value));
+		setCurrentPage(1);
+	}
+
+	function handleOrderName(e) {
+		e.preventDefault();
+		dispatch(orderByName(e.target.value));
+		setOrden(`Ordenado ${e.target.value}`);
+		setCurrentPage(1);
+	}
+
+	function handleOrderRating(e) {
+		e.preventDefault();
+		dispatch(orderByRating(e.target.value));
+		setOrden(`Ordenado ${e.target.value}`);
+		setCurrentPage(1);
+	}
+
+	return (
+		<div className={s.backg}>
+			<div>
+				{currentGames &&
+					currentGames.map((game) => {
+						return (
+							<Car
+								key={game.id}
+								id={game.id}
+								name={game.name}
+								background_image={game.background_image}
+								genres={game.genres}
+							/>
+						);
+					})}
+				,
+			</div>
+		</div>
+	);
 }
